dcd8939766edcad7a06fd3612f9a081a568906bc
frame88 <fleuzzi97@gmail.com>
2022-02-16 16:15:36 +0100

    instances

--- Movie.php
+++ Movie.php
@@ -7,20 +7,31 @@ metodi d’istanza che abbiamo visto stamattina e create un file Movie.php in cu
    => all’interno della classe è definito almeno un metodo
 - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle 
 relative proprietà -->
+
+
+
 <?php
 class Movie {
-    public $ titolo;
-    public $ durata;
-    public $ lingua;
+    public $title;
+    public $durata;
+    public $lingua;
+    public $regista;
+    public $anno;
 }
 
 $film = new Movie();
-$film->name = 'titanic';
+$film -> title = 'titanic';
 var_dump($film);
 
 $film_2 = new Movie();
-$film_2 -> name = 'jurassic park';
+$film_2 -> title = 'jurassic park';
 $film_2 -> durata = '120';
 $film_2 -> lingua = 'english';
 var_dump($film_2);
+
+$film_3 = new Movie();
+$film_3 -> title = 'narnia';
+$film_3 -> durata = '116';
+var_dump($film_3);
+
 ?>
